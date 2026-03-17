5b6f4036784d8b835dc50ad0f00bd1f017711546
fergarabato <fernando@3dar.com>
2026-03-17 10:20:29 -0300

    Trigger re-deploy and verify base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Menumatic/',
+  base: '/Menumatic/', // Aseguramos que coincida con el nombre del repo en el URL
 });
